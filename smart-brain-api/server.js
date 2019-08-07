404386646ec07cc9128025cde00aa0ac89550c15
Youssef Khaled <you@example.com>
2019-08-07 21:36:22 +0200

    Adding Postgres database

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        password : 'test',
+        database : 'smart-brain'
+    }
+});
+
 
 const app = express();
 
@@ -42,53 +54,71 @@ app.get('/',(req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-    console.log(req.body.email + '  ' + req.body.password);
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.json(database.users[0]);
-    } else {
-        res.status(400).json("error logging in");
-    }
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => res.json(user[0]))
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials');
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
 })
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    database.users.push({
-        id: 124,
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date(),
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        })
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    name: name,
+                    email: loginEmail[0],
+                    joined: new Date()
+            })
+            .then(user => res.json(user[0]))
+        })
+        .then(trx.commit)
+        .catch(trx.rollback)
     })
-    res.json(database.users[database.users.length-1]);
+    
+    .catch(err => res.status(400).json('unable to register!!'))
 })
 
 app.get('/profile/:userId', (req, res) => {
-    const { userId }  = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id ==  userId) {
-            found = true;
-            res.json(user);
-        } 
-    });
-    if (!found) {
-        res.status(404).json("no such user");
-    }
+    const { id }  = req.params;
+    db.select('*').from('users').where({ id })
+    .then( user => {
+        if (user.length) {
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not found!!');
+        }
+    })
+    .catch(err => res.status(400).json('error getting user!!'))
 })
 
 app.put('/image', (req, res) => {
-    const { userId }  = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id ==  userId) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
-    });
-    if (!found) {
-        res.status(404).json("no such user");
-    }
+    const { id }  = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 app.listen(3000, () => {
