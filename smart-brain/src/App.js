de3a08f267d3e83bfce5dc21d1f026bb4eed0020
youssef11khaled99 <uossefkhaled99@gmail.com>
2019-08-22 17:49:39 +0200

    deploy

--- smart-brain/src/App.js
+++ smart-brain/src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://frozen-crag-19910.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -103,7 +103,7 @@ class App extends Component {
     .then( response => {
       this.displayFaceBox(this.calculateFaceLocation(response))
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://frozen-crag-19910.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
